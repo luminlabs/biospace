35195312e08ae88e2c94b1d637ee67bbcb38544f
staxworks <matthewp.owens@gmail.com>
2013-08-29 16:15:35 -0700

    Initial commit with rails clean install and header/footer style

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biospace::Application.config.secret_token = 'd8302d7cb65c78655d22dc7a69a0be2a8cffcd4236b91ff069a74843e0944c99cbab8b8ab8319335072801e11148f1e310a457a32313f966151ac791d7d6b072'
