8ed76852976bc15a3437137f4140bcc7a373acaa
staxworks <matthewp.owens@gmail.com>
2013-08-30 15:13:52 -0700

    Adding linked-in login capability

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -225,8 +225,9 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-
+  require 'omniauth-linkedin'
+  config.omniauth :linkedin, ENV["APP_ID"], ENV["APP_SECRET"], :scope => 'r_basicprofile r_network r_emailaddress'
+ 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
